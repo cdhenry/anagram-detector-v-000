d2c41b888818d4a6270d50666ad6cbe119cadead
Chris Henry <cdhenry@gmail.com>
2018-02-13 16:56:41 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,13 +1,13 @@
 # Your code goes here!
 class Anagram
   attr_accessor :word, :anagrams
-  
+
   def initialize(word, anagrams)
     @word = word
     @anagrams = anagrams
   end
 
   def match
-    
+
   end
 end
